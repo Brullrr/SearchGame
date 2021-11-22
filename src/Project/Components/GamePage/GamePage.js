de2963808a7b33f0263f819af418bbaee9c56ba5
Brullrr <rbrull@yahoo.com>
2021-11-23 00:25:12 +0900

    troubleshoooting

--- src/Project/Components/GamePage/GamePage.js
+++ src/Project/Components/GamePage/GamePage.js
@@ -187,11 +187,10 @@ const GamePage = (props) => {
             charactersToFind.forEach(e => e.num === 1 ? characterOneAlive = true : null)
             if(imageNumber === 1 && characterOneAlive) {
 
-                alert('X is:   '   +  (clickedX - (window.innerWidth - imageWidth)) + 
+                alert('X is:   '   +  clickedX + 
                 '    Y is:   '  +  clickedY + 
                 '   image width is :  ' + (imageWidth) + 
-                '   inner width is:  ' + (window.innerWidth) +
-                '   client / windows inner width:  ' + (window.innerWidth - imageWidth))
+                '   inner width is:  ' + (window.innerWidth) )
                 
                 if(clickedX > 716 && clickedX < 739 && clickedY > 805 && clickedY < 836) {
                     
